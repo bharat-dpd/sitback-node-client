d293aa5040bee5ad67e35c3434e7aad42c4fbd6a
Samal <samal@treashare.in>
2014-08-02 21:14:57 +0530

    client fix

--- example/sample.js
+++ example/sample.js
@@ -1,13 +1,17 @@
 var sit = require("../");
-var b = sit({appId:123,appKey:"34e4",appSecret:"sdf343r$%F"});
+var b = sit({appId:"53d22fca5c651733174542bf",appKey:"34e4",appSecret:"sdf343r$%F"});
 
-b._createSignedString({method:"POST",path:"/send",message:["rrr"]});
+//b._createSignedString({method:"POST",path:"/send",message:["rrr"]});
 
 
 b.send({
-  email_id:"ffff",
-  template_tag:"signup",
+  sender:"samalgorai@gmail.com",
+  receiver:"samalgorai@gmail.com",
+  mail_type:"d",
   data:{
-
+   name:"Samal"
   }
+},function(err,body){
+  console.log(err,body);
+
 });
