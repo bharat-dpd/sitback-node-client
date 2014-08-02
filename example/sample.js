78d13f51b140b2d49f108009c3798849d32bd992
Samal <samal@treashare.in>
2014-08-02 22:13:44 +0530

    added web url

--- example/sample.js
+++ example/sample.js
@@ -1,13 +1,13 @@
 var sit = require("../");
-var b = sit({appId:"53d22fca5c651733174542bf",appKey:"34e4",appSecret:"sdf343r$%F"});
+var b = sit({appId:"53dc935398fb601229cf594b",appKey:"34e4",appSecret:"sdf343r$%F"});
 
 //b._createSignedString({method:"POST",path:"/send",message:["rrr"]});
 
 
 b.send({
-  sender:"samalgorai@gmail.com",
-  receiver:"samalgorai@gmail.com",
-  mail_type:"d",
+  sender:"samal@treashare.in",
+  receiver:"samal@treashare.in",
+  mail_type:"notfound",
   data:{
    name:"Samal"
   }
