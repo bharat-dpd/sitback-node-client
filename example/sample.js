4908db6cc1085cbe3e56f7cb3eb35fe5f3f1a026
bharat bhushan <bharat9728897555@gmail.com>
2014-10-19 20:08:20 +0530

    change appkey to token and create url in _post method instead of send

--- example/sample.js
+++ example/sample.js
@@ -1,5 +1,5 @@
 var sit = require("../");
-var b = sit({appId:"53dc935398fb601229cf594b",appKey:"34e4",appSecret:"sdf343r$%F"});
+var b = sit({appId:"53dc935398fb601229cf594b",token:"34e4",appSecret:"sdf343r$%F"});
 
 //b._createSignedString({method:"POST",path:"/send",message:["rrr"]});
 
@@ -10,7 +10,7 @@ b.send({
   mail_type:"Test",
   data:{
    name:"Samal",
-   random:Math.random()*10000000,
+   random:Math.random()*10000000
   }
 },function(err,body){
   console.log(err,body);
